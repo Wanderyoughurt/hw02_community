cb2eb1a6d6539677258494d49ad8a38777409e20
Александр Лаврешин <rwsnti2@mail.ru>
2021-04-25 22:59:09 +0300

    first iteration

---
+++ yatube/posts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
+    group = models.ForeignKey('Group', on_delete=models.CASCADE,
+                              related_name="posts", blank=True, null=True)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Имя")
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=200, verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
